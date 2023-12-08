bcf22376abf137c3c0623be7d71cf9e9774a8fdd
Nawaf Naofal <nawafnaufal1@gmail.com>
2023-12-09 02:20:41 +0700

    fix layout canvas

--- Assets/Scripts/Dialogue/OpeningDialogue.cs
+++ Assets/Scripts/Dialogue/OpeningDialogue.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class OpeningDialogue : MonoBehaviour
 {
@@ -17,5 +18,15 @@ public class OpeningDialogue : MonoBehaviour
     {
         // Memulai dialog
         DialogueManager.GetInstance()?.EnterDialogueMode(inkJSON);
+        StartCoroutine(WaitForDialogueToEnd());
+    }
+
+    private IEnumerator WaitForDialogueToEnd()
+    {
+        // Tunggu sampai dialog selesai
+        yield return new WaitUntil(() => !DialogueManager.GetInstance().dialogueIsPlaying);
+
+        // Jalankan perpindahan scene setelah dialog selesai
+        SceneManager.LoadScene("Island");
     }
 }
